0f41465f8e2e2117d76e8e170916d07cbb96e746
uTSAVsUBEDI <>
2021-04-01 22:33:26 -0700

    Pagination Added

--- my_firstapp/views.py
+++ my_firstapp/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (ListView, 
         DetailView, 
         CreateView,
         UpdateView,
         DeleteView)
+from django.contrib.auth.models import User
 
 from .models import Post
 
@@ -24,9 +25,22 @@ class PostListView(ListView):
     template_name = 'my_firstapp/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 3
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'my_firstapp/user_posts.html' #
+    context_object_name = 'posts'
+    
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(aurthor = user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
+
     
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
